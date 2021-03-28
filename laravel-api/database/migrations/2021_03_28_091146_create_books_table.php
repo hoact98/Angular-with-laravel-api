b12b3afc9d7f258a7457a2a4a913a8ff85a4134c
Hoa98 <chuthihoa98bgg@gmail.com>
2021-03-28 16:31:36 +0700

    update-database

---
+++ laravel-api/database/migrations/2021_03_28_091146_create_books_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->unsignedBigInteger('categoyId');
+            $table->foreign('categoyId')->references('id')->on('categories');
+            $table->unsignedBigInteger('authorId');
+            $table->foreign('authorId')->references('id')->on('authors');
+            $table->text('detail');
+            $table->text('short_desc')->nullable();
+            $table->string('image',191)->default('images/default.jpg');
+            $table->integer('price')->default(0);
+            $table->integer('promotion_price')->default(0);
+            $table->integer('views')->default(0);  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
---
+++ laravel-api/database/migrations/2021_03_28_091256_create_galleries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGalleriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galleries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('bookId');
+            $table->foreign('bookId')->references('id')->on('books');
+            $table->string('image',191)->default('images/default.jpg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galleries');
+    }
+}
